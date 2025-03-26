daf069dd27ce874f2c2a4a16d9513c67c770028c
AlbaCastilla <castillamacki@gmail.com>
2025-03-26 09:29:18 +0100

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/*<body>{children}</body>*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      {/*.className is how Next.js dynamically attaches the generated CSS class for the font, it creates an automatic class to attach, for example_inter123 and stores it in ${inter.className */}
+      {/*antialiased makes sure text doesnt look pixeled and smoothes out with the backround color */}
     </html>
   );
 }
